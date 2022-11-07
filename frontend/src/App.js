248fd321e67c4cdddb979d57338759ac26299542
taic-dev <c.t.o.taishi.0530@gmail.com>
2022-11-07 14:29:05 +0900

    地図の表示/マーク表示練習

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,42 @@
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  GoogleMap,
+  Marker,
+  InfoWindow,
+  useLoadScript,
+  LoadScript,
+} from "@react-google-maps/api";
 
-function App() {
+const App = () => {
+
+  const mapStyles = {
+    height: "100vh",
+    width: "100vw",
+  };
+  const center = {
+    lat: 35.69575,
+    lng: 139.77521,
+  };
+  
+  const positionAkiba = {
+    lat: 35.69731,
+    lng: 139.7747,
+  };
+  
+  const positionIwamotocho = {
+    lat: 35.69397,
+    lng: 139.7762,
+  };
+
   return (
-    <div>
-      WC-Map
-    </div>
+    <LoadScript googleMapsApiKey="AIzaSyD7Tvp_twEILZF9ia8LxblM4J02J1qGv3U">
+      <GoogleMap mapContainerStyle={mapStyles} center={center} zoom={17}>
+        <Marker position={positionAkiba} />
+        <Marker position={positionIwamotocho} />
+      </GoogleMap>
+    </LoadScript>
   );
-}
+};
 
 export default App;
