0c95deb8dcb0097b308ad0876ebe7da28ec28681
taic-dev <c.t.o.taishi.0530@gmail.com>
2022-11-07 15:49:23 +0900

    マーカーが表示されなかったのでStrictModeを解除

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import {
   GoogleMap,
@@ -7,8 +7,30 @@ import {
   useLoadScript,
   LoadScript,
 } from "@react-google-maps/api";
+import { useEffect } from "react";
 
 const App = () => {
+  const [position, setPosition] = useState([]);
+
+  useEffect(() => {
+    // DBから登録した地図情報を取得
+    const postiion = setPosition([
+      {
+        id: 1,
+        base: {
+          lat: 35.69731,
+          lng: 139.7747,
+        },
+      },
+      {
+        id: 2,
+        base: {
+          lat: 35.69397,
+          lng: 139.7762,
+        },
+      }
+    ]);
+  }, []);
 
   const mapStyles = {
     height: "100vh",
@@ -18,22 +40,18 @@ const App = () => {
     lat: 35.69575,
     lng: 139.77521,
   };
-  
-  const positionAkiba = {
-    lat: 35.69731,
-    lng: 139.7747,
-  };
-  
-  const positionIwamotocho = {
-    lat: 35.69397,
-    lng: 139.7762,
-  };
 
   return (
     <LoadScript googleMapsApiKey="AIzaSyD7Tvp_twEILZF9ia8LxblM4J02J1qGv3U">
       <GoogleMap mapContainerStyle={mapStyles} center={center} zoom={17}>
-        <Marker position={positionAkiba} />
-        <Marker position={positionIwamotocho} />
+        {
+          // setTimeout(()=>{
+            position.map((value) => {
+              console.log(33333)
+              return <Marker position={value.base} key={value.id}/>;
+            })
+          // },4000)
+        }
       </GoogleMap>
     </LoadScript>
   );
