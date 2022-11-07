8a830020b0fc772354668e2a6a1c8028d926b515
taic-dev <c.t.o.taishi.0530@gmail.com>
2022-11-08 00:49:23 +0900

    現在地の取得

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,59 +1,98 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import {
   GoogleMap,
   Marker,
   InfoWindow,
   useLoadScript,
-  LoadScript,
 } from "@react-google-maps/api";
-import { useEffect } from "react";
+
+import {
+  BottomNavigation,
+  BottomNavigationAction,
+  Box,
+  IconButton,
+} from "@mui/material";
+
+import GpsFixedIcon from "@mui/icons-material/GpsFixed";
+import RestoreIcon from "@mui/icons-material/Restore";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
 
 const App = () => {
-  const [position, setPosition] = useState([]);
+  const [value, setValue] = React.useState(0);
+  const [currentLocation, setCurrentLocation] = React.useState({
+    lat: 35.681236,
+    lng: 139.767125,
+  });
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: "AIzaSyD7Tvp_twEILZF9ia8LxblM4J02J1qGv3U",
+  });
 
-  useEffect(() => {
-    // DBから登録した地図情報を取得
-    const postiion = setPosition([
-      {
-        id: 1,
-        base: {
-          lat: 35.69731,
-          lng: 139.7747,
-        },
-      },
-      {
-        id: 2,
-        base: {
-          lat: 35.69397,
-          lng: 139.7762,
-        },
-      }
-    ]);
-  }, []);
+  if (!isLoaded) return <div>Loading...</div>;
 
   const mapStyles = {
-    height: "100vh",
-    width: "100vw",
+    height: "100%",
+    width: "100%",
   };
-  const center = {
-    lat: 35.69575,
-    lng: 139.77521,
+
+  const defaultCenter = {
+    lat: 35.681236,
+    lng: 139.767125,
+  };
+
+  // 現在地の取得
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setCurrentLocation({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        });
+        console.log(position.coords);
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   };
 
   return (
-    <LoadScript googleMapsApiKey="AIzaSyD7Tvp_twEILZF9ia8LxblM4J02J1qGv3U">
-      <GoogleMap mapContainerStyle={mapStyles} center={center} zoom={17}>
-        {
-          // setTimeout(()=>{
-            position.map((value) => {
-              console.log(33333)
-              return <Marker position={value.base} key={value.id}/>;
-            })
-          // },4000)
-        }
-      </GoogleMap>
-    </LoadScript>
+    <>
+      <main className="main">
+        <GoogleMap
+          mapContainerStyle={mapStyles}
+          zoom={14}
+          center={currentLocation}
+        >
+          <Marker position={currentLocation} />
+        </GoogleMap>
+
+        <div className="btn-wrapper">
+          <IconButton
+            style={{ width: "80px", height: "80px" }}
+            color="primary"
+            component="label"
+            onClick={getCurrentLocation}
+          >
+            <GpsFixedIcon style={{ width: "40px", height: "40px" }} />
+          </IconButton>
+        </div>
+      </main>
+      <footer className="footer">
+        <BottomNavigation
+          showLabels
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        >
+          <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+          <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+          <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+        </BottomNavigation>
+      </footer>
+    </>
   );
 };
 
