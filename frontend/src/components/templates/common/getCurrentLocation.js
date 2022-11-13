03bae4ab4b92162860a522613bfa5722916af6e5
taic-dev <c.t.o.taishi.0530@gmail.com>
2022-11-13 21:43:56 +0900

    詳細情報の画面 大枠完成

--- frontend/src/components/templates/common/getCurrentLocation.js
+++ frontend/src/components/templates/common/getCurrentLocation.js
@@ -4,34 +4,39 @@ const getCurrentLocation = () => {
     return;
   }
 
+  let coordinate = { lat: 35.681236, lng: 139.767125 };
+
   return new Promise((resolve, reject) => {
     navigator.geolocation.getCurrentPosition(
-        (position) => {  
-          let coordinate = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };  
-          resolve(coordinate);
-        },
-        (error) => {
-          switch (error.code) {
-            case 1: //PERMISSION_DENIED
-              alert("位置情報の利用が許可されていません");
-              break;
-            case 2: //POSITION_UNAVAILABLE
-              alert("現在位置が取得できませんでした");
-              break;
-            case 3: //TIMEOUT
-              alert("タイムアウトになりました");
-              break;
-            default:
-              alert("その他のエラー(エラーコード:" + error.code + ")");
-              break;
-          }
+      (position) => {
+        coordinate = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+        resolve(coordinate);
+      },
+      (error) => {
+        switch (error.code) {
+          case 1: //PERMISSION_DENIED
+            alert("位置情報の利用が許可されていません");
+            reject(coordinate);
+            break;
+          case 2: //POSITION_UNAVAILABLE
+            alert("現在位置が取得できませんでした");
+            reject(coordinate);
+            break;
+          case 3: //TIMEOUT
+            alert("タイムアウトになりました");
+            reject(coordinate);
+            break;
+          default:
+            alert("その他のエラー(エラーコード:" + error.code + ")");
+            reject(coordinate);
+            break;
         }
-      )
+      }
+    );
   });
-
 };
 
 export default getCurrentLocation;
