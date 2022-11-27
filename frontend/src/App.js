ffb857a7d2adde154b942abff7c881a8e82a8d9d
taic-dev <c.t.o.taishi.0530@gmail.com>
2022-11-27 11:00:19 +0900

    ログインページの大枠完成

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 // components
 import LocationMain from "./components/pages/location/LocationMain";
 import LoginMain from "./components/pages/login/LoginMain";
-import Footer from "./components/pages/common/Footer";
 
 // reset
 import { Reset } from "styled-reset";
@@ -22,7 +21,6 @@ const App = () => {
           <Route path={"/"} element={<LocationMain />} />
           <Route path={"/login"} element={<LoginMain />} />
         </Routes>
-        <Footer />
       </BrowserRouter>
     </>
   );
