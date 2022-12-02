9ab680021fea8689646bf167d9f3f87c8ce19216
taic-dev <c.t.o.taishi.0530@gmail.com>
2022-12-02 09:01:20 +0900

    authをlocalStrageで管理

--- frontend/src/components/pages/admin/AdminMain.js
+++ frontend/src/components/pages/admin/AdminMain.js
@@ -2,13 +2,13 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 const AdminMain = () => {
-    const auth = useSelector((state) => state.auth);
+  const auth = useSelector((state) => state.auth);
   return (
-        <main>
-            管理者ページ
-            {console.log(auth)}
-        </main>
-    );
+    <main>
+      管理者ページ
+      {console.log(auth)}
+    </main>
+  );
 };
 
 export default AdminMain;
