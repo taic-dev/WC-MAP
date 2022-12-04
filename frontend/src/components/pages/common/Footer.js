be64f6e42feac944396861b527c02bc394b9c3e3
taic-dev <c.t.o.taishi.0530@gmail.com>
2022-12-04 17:07:10 +0900

    AdminFooterの内容変更

--- frontend/src/components/pages/common/Footer.js
+++ frontend/src/components/pages/common/Footer.js
@@ -1,15 +1,16 @@
 import React, { useState } from "react";
-import {
-  BottomNavigation,
-  BottomNavigationAction,
-} from "@mui/material";
+import { BottomNavigation, BottomNavigationAction } from "@mui/material";
 
 import RestoreIcon from "@mui/icons-material/Restore";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
+import HomeIcon from "@mui/icons-material/Home";
+import LogoutIcon from "@mui/icons-material/Logout";
+import PostAddIcon from "@mui/icons-material/PostAdd";
 
 const Footer = () => {
   const [value, setValue] = useState(0);
+  const admin = localStorage.getItem("admin");
 
   return (
     <footer className="footer">
