e59e8d3d979efb326701c66b829be5dac378764a
taic-dev <c.t.o.taishi.0530@gmail.com>
2022-12-12 23:56:05 +0900

    loading画面の修正

--- frontend/src/components/pages/location/Loading.js
+++ frontend/src/components/pages/location/Loading.js
@@ -1,22 +1,33 @@
-import { Box, CircularProgress, } from "@mui/material";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { Box } from "@mui/material";
+import LinearProgress from "@mui/material/LinearProgress";
 
 const Loading = () => {
   return (
     <Box
-      sx={{ width: 450, height: "100vh", margin: "auto", position: "relative" }}
+      sx={{
+        width: "100%",
+        height: "100vh",
+        margin: "auto",
+        position: "relative",
+      }}
     >
       <div
         style={{
           position: "absolute",
-          top: "50%",
+          top: "40%",
           left: "50%",
           width: "100%",
           transform: "translate(-50%, -50%)",
-          textAlign: "center"
+          textAlign: "center",
         }}
       >
-      <CircularProgress />
+        <img
+          src={`${process.env.PUBLIC_URL}/img/logo.png`}
+          alt="ロゴ画像"
+          style={{ width: "100%" }}
+        />
+        <LinearProgress color="inherit" sx={{ width: "60%", margin: "auto" }} />
       </div>
     </Box>
   );
