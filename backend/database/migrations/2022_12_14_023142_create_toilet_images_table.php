077e705248a20db4986246a7545a6f5ba719cdc3
taic-dev <c.t.o.taishi.0530@gmail.com>
2022-12-14 11:46:30 +0900

    migrationファイルの作成

---
+++ backend/database/migrations/2022_12_14_023142_create_toilet_images_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateToiletImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('toilet_images', function (Blueprint $table) {
+            $table->increments("id");
+            $table->biginteger("toilet_id")->unique();
+            $table->string("image_url");
+            $table->timestamp("deleted_at")->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('toilet_images');
+    }
+}
