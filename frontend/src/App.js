a6b5d72441a4ad99df528f7968172f9b002e50f9
taic-dev <c.t.o.taishi.0530@gmail.com>
2023-01-08 22:40:56 +0900

    微修正

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,30 +12,40 @@ import SignupMain from "./components/pages/signup/SignupMain";
 import AdminMain from "./components/pages/admin/AdminMain";
 import PostMain from "./components/pages/post/PostMain";
 import ArchiveMain from "./components/pages/archive/ArchiveMain";
-import { useAuth } from "./components/hooks/useAuth"; 
+import { useAuth } from "./components/hooks/useAuth";
 import Loading from "./components/pages/location/Loading";
 
 const App = () => {
-
   const auth = useAuth();
-  
+
+  // ログイン状態にあるか確認できるまでは何もしない。
+  if (auth === false) return;
+
   return (
     <>
-    { auth === false ? <Loading /> : (
-      <>
-        <Reset />
-        <BrowserRouter>
-          <Routes>
-            <Route path={"/"} element={<LocationMain />} />
-            <Route path={"/signup"} element={<SignupMain />} />
-            <Route path={"/login"} element={ auth ? <Navigate to="/admin" /> : <LoginMain />} />
-            <Route path={"/admin"} element={ auth ? <AdminMain /> : <Navigate to="/login" />} />
-            <Route path={"/post"} element={ auth ? <PostMain /> : <Navigate to="/login" />} />
-            <Route path={"/archive"} element={ auth ? <ArchiveMain /> : <Navigate to="/login" />} />
-          </Routes>
-        </BrowserRouter>
-      </>
-    ) }
+      <Reset />
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<LocationMain />} />
+          <Route path={"/signup"} element={<SignupMain />} />
+          <Route
+            path={"/login"}
+            element={auth ? <Navigate to="/admin" /> : <LoginMain />}
+          />
+          <Route
+            path={"/admin"}
+            element={auth ? <AdminMain /> : <Navigate to="/login" />}
+          />
+          <Route
+            path={"/post"}
+            element={auth ? <PostMain /> : <Navigate to="/login" />}
+          />
+          <Route
+            path={"/archive"}
+            element={auth ? <ArchiveMain /> : <Navigate to="/login" />}
+          />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
