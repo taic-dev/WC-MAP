badc1588cd27ca2d86420f44aa4c627387300599
taic-dev <c.t.o.taishi.0530@gmail.com>
2023-01-10 11:40:07 +0900

    #37 共通アイコンのコンポーネント作成

--- frontend/src/components/pages/recents/RecentsMain.js
+++ frontend/src/components/pages/recents/RecentsMain.js
@@ -1,8 +1,13 @@
 import React from "react";
 import Header from "../common/Header";
 import Footer from "../common/Footer";
-import { Avatar, Divider, List, ListItem, ListItemAvatar, ListItemText, Typography } from "@mui/material";
+import { Divider, List, ListItem, ListItemText, Typography } from "@mui/material";
 import { Box } from "@mui/system";
+import AutoAwesomeIcon from "@mui/icons-material/AutoAwesome";
+import AccessibleIcon from "@mui/icons-material/Accessible";
+import CurrencyYenIcon from "@mui/icons-material/CurrencyYen";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDroplet } from "@fortawesome/free-solid-svg-icons";
 
 const RecentsMain = () => {
   return (
@@ -26,11 +31,17 @@ const RecentsMain = () => {
                   >
                     詳細情報詳細情報詳細情報詳細情報詳細情報詳細情報…
                   </Typography>
-                  <List sx={{ display: "flex",  }}>
-                    <ListItem sx={{ width: "fit-content" }}>icon</ListItem>
-                    <ListItem sx={{ width: "fit-content" }}>icon</ListItem>
-                    <ListItem sx={{ width: "fit-content" }}>icon</ListItem>
-                  </List>
+                  {/* Marker部分と同じ */}
+                  <ul className="common__icon">
+                    { <li><AutoAwesomeIcon style={{ color: "#ffd700", width: "15px" }} /></li> }
+                    { <li><CurrencyYenIcon style={{ width: "15px" }} /></li> }
+                    { <li><FontAwesomeIcon icon={faDroplet} style={{ color : '#1E90FF' }} /></li> }
+                    { <li><AccessibleIcon style={{ color: '#008000', width: "20px" }} /></li> }
+                    {/* { marker.cleanliness == "非常に綺麗" && <li><AutoAwesomeIcon style={{ color: "#ffd700", width: "15px" }} /></li> } */}
+                    {/* { marker.price == "有料" && <li><CurrencyYenIcon style={{ width: "15px" }} /></li> } */}
+                    {/* { marker.is_washlet == 1 && <li><FontAwesomeIcon icon={faDroplet} style={{ color : '#1E90FF' }} /></li> } */}
+                    {/* { marker.is_multi_purpose_room == 1 && <li><AccessibleIcon style={{ color: '#008000', width: "20px" }} /></li> } */}
+                  </ul>
                 </React.Fragment>
               }
             />
@@ -50,11 +61,17 @@ const RecentsMain = () => {
                   >
                     詳細情報詳細情報詳細情報詳細情報詳細情報詳細情報…
                   </Typography>
-                  <List sx={{ display: "flex",  }}>
-                    <ListItem sx={{ width: "fit-content" }}>icon</ListItem>
-                    <ListItem sx={{ width: "fit-content" }}>icon</ListItem>
-                    <ListItem sx={{ width: "fit-content" }}>icon</ListItem>
-                  </List>
+                  {/* Marker部分と同じ */}
+                  <ul className="common__icon">
+                    { <li><AutoAwesomeIcon style={{ color: "#ffd700", width: "15px" }} /></li> }
+                    { <li><CurrencyYenIcon style={{ width: "15px" }} /></li> }
+                    { <li><FontAwesomeIcon icon={faDroplet} style={{ color : '#1E90FF' }} /></li> }
+                    { <li><AccessibleIcon style={{ color: '#008000', width: "20px" }} /></li> }
+                    {/* { marker.cleanliness == "非常に綺麗" && <li><AutoAwesomeIcon style={{ color: "#ffd700", width: "15px" }} /></li> } */}
+                    {/* { marker.price == "有料" && <li><CurrencyYenIcon style={{ width: "15px" }} /></li> } */}
+                    {/* { marker.is_washlet == 1 && <li><FontAwesomeIcon icon={faDroplet} style={{ color : '#1E90FF' }} /></li> } */}
+                    {/* { marker.is_multi_purpose_room == 1 && <li><AccessibleIcon style={{ color: '#008000', width: "20px" }} /></li> } */}
+                  </ul>
                 </React.Fragment>
               }
             />
